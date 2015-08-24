d46b15b85861c1676cc3e3cab1961abc46dd79f0
Tobias Olry <tobias.olry@gmail.com>
2015-08-24 23:30:47 +0200

    first tests with backbone.facetr

--- web/js/app/views.js
+++ web/js/app/views.js
@@ -15,6 +15,29 @@ var SearchView = Backbone.View.extend({
     render: function() {
         this.$el.html("");
 
+        var facetCollection = Facetr(this.collection);
+
+        var facetVotes = facetCollection.facet('votes');
+        var facetSpeakers = facetCollection.facet('speakers');
+        var facetComments = facetCollection.facet('comments');
+
+        _.each(facetCollection.facets(), function(facet) {
+            var facet = facet.toJSON();
+
+            this.$el.append('<h2>' + facet.data.label + '</h2>');
+
+            var $ul = $('<ul/>');
+            this.$el.append($ul);
+
+            _.each(facet.values, function(item) {
+                $li = $("<li>" + item.value + " [" + item.count + "]</li>");
+                $ul.append($li);
+            }, this);
+
+        }, this);
+
+        facetCollection.sortBy('votes').desc();
+
         _.each(this.collection.models, function (topic) {
             var topic = topic.toJSON();
             this.$el.append('<p>' + topic.html.short + '</p>');
