c5d55a02746e657df3f383647e0ab0e8b6c33bda
David Badura <david.badura@i22.de>
2016-05-03 19:31:28 +0200

    assets management rewrite

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
+    browserSync = require('browser-sync').create()
+    ;
+
+gulp.task('default', ['css', 'js', 'fonts']);
+
+gulp.task('css', function () {
+    gulp.src(['src/scss/app.scss'])
+        .pipe(concat('app.scss'))
+        .pipe(sass({includePaths: [
+            'bower_components/foundation/scss/',
+            'bower_components/font-awesome/scss/'
+        ], errLogToConsole: true}))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('web/css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function () {
+    gulp.src([
+            'bower_components/modernizr/modernizr.js',
+            'bower_components/angular/angular.min.js',
+            'bower_components/angular-resource/angular-resource.min.js',
+            'bower_components/moment/min/moment.min.js',
+            'bower_components/angular-moment/angular-moment.min.js',
+            'bower_components/jquery/dist/jquery.min.js',
+            'bower_components/foundation/js/foundation.min.js',
+            'bower_components/headroom.js/dist/headroom.js',
+            'bower_components/headroom.js/dist/angular.headroom.js',
+            'src/js/services.js',
+            'src/js/directives.js',
+            'src/js/app.js',
+            'src/js/app.markdown-preview.js'
+        ])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('web/js/'))
+        .pipe(browserSync.stream());
+
+    // copy jquery.min.map
+    gulp.src('node_modules/jquery/dist/jquery.min.map')
+        .pipe(gulp.dest('web/js/'));
+});
+
+gulp.task('fonts', function () {
+    gulp.src('bower_components/font-awesome/fonts/*')
+        .pipe(gulp.dest('web/fonts/font-awesome'));
+});
+
+gulp.task('watch', ['default'], function () {
+    browserSync.init({
+        proxy: 'localhost',
+        open: false
+    });
+
+    gulp.watch('src/scss/*.scss', ['css']);
+    gulp.watch('src/js/*.js', ['js']);
+    gulp.watch('src/**/*.html.twig').on('change', browserSync.reload);
+});
