b41b17274d098dfdbcadbd425343707a8822f8ae
David Badura <david.badura@i22.de>
2016-11-10 22:05:26 +0100

    display auth errors

--- src/js/components/Security/Login.jsx
+++ src/js/components/Security/Login.jsx
@@ -6,58 +6,73 @@ import LoginForm from "./LoginForm";
 import {token} from "../../services/Parameters";
 
 export default class Index extends React.Component {
-    submit(data) {
-        Client.post('/api/login', {
-            username: data.username,
-            password: data.password
-        }).then(response => {
-            this.authenticate(response.data.token);
-        }).catch(function () {
-            AuthorizationStorage.clear();
-        });
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: false
+    };
+  }
+
+  submit(data) {
+    Client.post('/api/login', {
+      username: data.username,
+      password: data.password
+    }).then(response => {
+      this.authenticate(response.data.token);
+    }).catch(() => {
+      this.setState({error: true});
+
+      AuthorizationStorage.clear();
+    });
+  }
+
+  componentDidMount() {
+    if (token) {
+      this.authenticate(token);
+    }
+  }
+
+  authenticate(token) {
+    AuthorizationStorage.setToken(token);
+
+    Client.get('/api/user/current').then(function (response) {
+      AuthorizationStorage.setUser(response.data);
+
+      let redirect = localStorage.getItem('redirect');
+      localStorage.removeItem('redirect');
+
+      if (!response.data.name || !response.data.email) {
+        History.push('/user/' + response.data.id + "/edit");
+      } else if (redirect) {
+        History.push(redirect);
+      } else {
+        History.push('/');
+      }
+
+    }).catch(() => {
+      this.setState({error: true});
+
+      AuthorizationStorage.clear();
+    });
+  }
+
+  render() {
+    if (token) {
+      return null;
     }
 
-    componentDidMount() {
-        if (token) {
-            this.authenticate(token);
-        }
-    }
+    console.log(this.state.error);
 
-    authenticate(token) {
-        AuthorizationStorage.setToken(token);
-
-        Client.get('/api/user/current').then(function (response) {
-            AuthorizationStorage.setUser(response.data);
-
-            let redirect = localStorage.getItem('redirect');
-            localStorage.removeItem('redirect');
-
-            if (!response.data.name || !response.data.email) {
-                History.push('/user/' + response.data.id + "/edit");
-            } else if (redirect) {
-                History.push(redirect);
-            } else {
-                History.push('/');
-            }
-
-        }).catch(function () {
-            AuthorizationStorage.clear();
-        });
-    }
-
-    render() {
-        if (token) {
-            return null;
-        }
-
-        return (
-            <main>
-                <div className="row">
-                    <div className="medium-6 medium-centered large-4 large-centered columns">
-                        <LoginForm submit={this.submit.bind(this)}/>
-                    </div>
-                </div>
-            </main>
-        );
-    }
+    return (
+      <main>
+        <div className="row">
+          <div className="medium-6 medium-centered large-4 large-centered columns">
+            {this.state.error ? <div className="alert-box alert">wrong credentials</div> : null}
+            <LoginForm submit={this.submit.bind(this)}/>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
